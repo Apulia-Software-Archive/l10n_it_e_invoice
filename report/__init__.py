76e4b5fa5f985b043c4a8569230bcfdd19e6e8b9
Francesco Apruzzese <cescoap@gmail.com>
2014-10-24 13:12:30 +0200

    [ADD] RML Report to use as invoice pdf [IMP] default report to call in journal in module installation

--- report/__init__.py
+++ report/__init__.py
@@ -21,27 +21,5 @@
 #
 ##############################################################################
 
-{
-    'name': "Electronic Invoice",
-    'version': '0.1',
-    'category': 'Account',
-    'description': """
-Electronic Invoice Management - Italian Law
-Modulo di gestione fatturazione elettronica""",
-    'author': 'Apulia Software srl <info@apuliasoftware.it>',
-    'website': 'www.apuliasoftware.it',
-    'license': 'AGPL-3',
-    "depends": ['account', ],
-    "data": [
-        'security/security.xml',
-        'security/ir.model.access.csv',
-        'partner/partner_view.xml',
-        'company/company_view.xml',
-        'account/account_view.xml',
-        'report/account_report.xml',
-        'account/e-invoice_data.xml',
-        'wizard/send_invoice_view.xml',
-        ],
-    "active": False,
-    "installable": True
-}
+
+import account_print_invoice
--- report/account_print_invoice.py
+++ report/account_print_invoice.py
@@ -2,14 +2,12 @@
 ##############################################################################
 #
 #    OpenERP, Open Source Management Solution
-#    Copyright (c) 2014 Apulia Software All Rights Reserved.
-#                       www.apuliasoftware.it
-#                       info@apuliasoftware.it
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -21,8 +19,23 @@
 #
 ##############################################################################
 
-import company
-import partner
-import account
-import wizard
-import report
+
+import time
+from report import report_sxw
+
+
+class account_invoice(report_sxw.rml_parse):
+
+    def __init__(self, cr, uid, name, context):
+        super(account_invoice, self).__init__(cr, uid, name, context=context)
+        self.localcontext.update({
+            'time': time,
+        })
+
+
+report_sxw.report_sxw(
+    'report.account.e_invoice',
+    'account.invoice',
+    'l10n_it_e_invoice/report/account_print_e_invoice.rml',
+    parser=account_invoice
+)
